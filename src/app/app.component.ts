90d6778678e16248725ff21a34069751269d9d74
hyzenberg22 <hyzenberg22@protonmail.com>
2023-06-15 19:10:15 +0530

    added the product management page to root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
 // imported packages
 import { Component } from '@angular/core';
 
-
-
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css'],
+  template: `
+    <div>
+      <h1>{{ pageTitle }}</h1>
+      <!-- add the products page  -->
+      <pm-products></pm-products>
+    </div>
+  `,
 })
 export class AppComponent {
   pageTitle = 'Acme Product Management';
 }
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ProductListCoomponent } from './Products/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListCoomponent
   ],
   imports: [
     BrowserModule
